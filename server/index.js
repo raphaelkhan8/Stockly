35671740b5384827f71fcf69e0e24c68ff4b664b
Raphael Khan <raphaelkhan8@gmail.com>
2021-04-24 14:33:00 -0500

    using websockets to transmit python code to client

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const { stockInfoRouter } = require('./routes/stockInfoRouter');
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
+const { callToPythonAsync } = require("./utils/helpers");
+
 const app = express();
 const port = 3001;
 
 app.use(cors());
-app.use(stockInfoRouter);
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("client connected");
+  socket.on("data-fetch", async (ticker) => {
+    const data = await callToPythonAsync("./stock_summarizer.py", ticker);
+    const stringifiedData = data.toString();
+    socket.emit("data-ready", stringifiedData);
+  });
+  socket.on('disconnection', () => {
+    console.log('Client disconnected');
+  })
+});
+
+server.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
