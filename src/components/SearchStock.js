bc34b34657164a60b7a480f3c47fe6eaa1ae6a3a
Raphael Khan <raphaelkhan8@gmail.com>
2021-04-24 14:39:02 -0500

    added Spinner and Textloop

--- src/components/SearchStock.js
+++ src/components/SearchStock.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+import { Spinner } from 'react-bootstrap'
+import TextLoop from "react-text-loop";
 
-const SearchStock = ({ searchStock }) => {
+const SearchStock = ({ searchStock, loading }) => {
   const [ticker, setText] = useState('')
 
   const onSubmit = (e) => {
@@ -22,13 +24,26 @@ const SearchStock = ({ searchStock }) => {
         <label>Ticker</label>
         <input
           type='text'
-          placeholder='Stock or Cryto ticker symbol'
+          placeholder={loading ? 'Fetching data...this will take a few minutes' : 'Stock or Cryto ticker symbol'}
           value={ticker}
           onChange={(e) => setText(e.target.value)}
+          disabled={loading ? true : false }
         />
       </div>
 
-      <input type='submit' value='Search Stock' className='btn btn-block' />
+      <button type='submit' 
+        className='btn btn-block' > {loading ?
+          (<Spinner animation="border" variant="info" role="status"><span className="sr-only">
+            <TextLoop interval={20000}>
+              <div>Searching for stock news...</div>
+              <div>Scraping news links...</div>
+              <div>Summarizing articles...</div>
+              <div>Calculating sentiment...</div>
+              <div>Gathering results...</div>
+            </TextLoop>
+            </span>
+            </Spinner>)
+          :'Search Stock'} </button>
     </form>
   )
 }
