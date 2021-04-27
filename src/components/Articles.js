eedddf0bec534a32b42359d5c3a6f4c5d7d43196
Raphael Khan <raphaelkhan8@gmail.com>
2021-04-27 15:28:22 -0500

    extracted Article into its own component

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,23 +1,10 @@
-import { FaSave } from 'react-icons/fa'
+import Article from './Article'
 
-const Articles = ({ articles, saveArticle }) => {
+const Articles = ({ articles, saveArticle, deleteArticle }) => {
   return (
     <>
-      {articles.map((article) => (
-        <div className='stock'>
-        <h3>
-          Ticker: {article.symbol.toUpperCase()}{' '}
-          <FaSave
-            style={{ color: 'green', cursor: 'pointer' }}
-            title="Save"
-            onClick={() => saveArticle(article)}
-          />
-        </h3>
-        <p><b>Summary:</b> {article.summary}</p>
-        <p><b>Sentiment:</b> {article.sentiment}</p>
-        <p><b>Score:</b> {article.score}</p>
-        <p><b>Article:</b> {<a href={article.url} target="_blank" rel="noreferrer noopener">Click to Open in New Tab</a>}</p>
-      </div>
+      {articles.map((article, i) => (
+        <Article key={i} article={article} saveArticle={saveArticle} deleteArticle={deleteArticle} origin={"Articles"} />
       ))}
     </>
   )
